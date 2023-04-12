cc25a7838aa1624e0a976de722b1edfe8a587ef6
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-12 20:17:19 +0530

    ADDED:code to edit

--- AddressBook.java
+++ AddressBook.java
@@ -43,6 +43,52 @@ public class AddressBook {
         obj2.setPhoneno(obj.nextInt());
         System.out.println("enter the  Email");
         obj2.setEmail(obj.next());
+        System.out.println("Option to edit");
+        System.out.println("1.FirstName \n 2.Lastname \n 3.Address \n 4.city \n 5.state \n 6. zipcode \n 7. Phoneno \n 8.Email ");
+
+
+
+        Scanner obj4 =new Scanner(System.in);
+        System.out.println("Enter the option  ");
+        int option =obj.nextInt();
+        switch(option){
+            case 1:
+                System.out.println("Enter the First name");
+                obj1.setFirstname(obj.next());
+                break;
+            case 2:
+                System.out.println("Enter the Last name");
+                obj1.setLastname(obj.next());
+                break;
+            case 3:
+                System.out.println("Enter the Address");
+                obj1.setAddress(obj.next());
+                break;
+            case 4:
+                System.out.println("Enter the city");
+                obj1.setCity(obj.next());
+                break;
+            case 5:
+                System.out.println("Enter the State");
+                obj1.setState(obj.next());
+                break;
+            case 6:
+                System.out.println("Enter the Zipcode");
+                obj1.setZipcode(obj.nextInt());
+                break;
+            case 7:
+                System.out.println("Enter the Phoneno");
+                obj1.setPhoneno(obj.nextInt());
+                break;
+            case 8:
+                System.out.println("Enter the Email");
+                obj1.setEmail(obj.next());
+                break;
+
+
+        }
+
+        System.out.println("Changes " +obj1);
 
 
     }
